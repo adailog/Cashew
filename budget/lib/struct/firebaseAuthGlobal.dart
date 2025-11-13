89f3c117a5f2c51870b66a0dd67dac0505a38b5e
adailog <118139709+adailog@users.noreply.github.com>
2025-11-14 00:30:13 +0800

    Update firebaseAuthGlobal.dart

--- budget/lib/struct/firebaseAuthGlobal.dart
+++ budget/lib/struct/firebaseAuthGlobal.dart
@@ -1,16 +1,20 @@
 import 'package:budget/struct/settings.dart';
 import 'package:budget/widgets/accountAndBackup.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// Firebase相关导入已删除
+// import 'package:firebase_auth/firebase_auth.dart';
 // Google Sign In导入已删除
 // import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
+// import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
 
-OAuthCredential? _credential;
+// OAuthCredential类型已被删除
+// OAuthCredential? _credential;
 
-Future<FirebaseFirestore?> firebaseGetDBInstanceAnonymous() async {
+// Firebase Firestore类型已被替换为dynamic
+Future<dynamic> firebaseGetDBInstanceAnonymous() async {
   try {
-    await FirebaseAuth.instance.signInAnonymously();
-    return FirebaseFirestore.instance;
+    // Firebase匿名登录功能已被禁用
+    print("Firebase匿名登录功能已被禁用");
+    return null;
   } catch (e) {
     print("There was an error with firebase login");
     print(e.toString());
@@ -19,7 +23,7 @@ Future<FirebaseFirestore?> firebaseGetDBInstanceAnonymous() async {
 }
 
 // Google Drive功能已被禁用
-Future<FirebaseFirestore?> firebaseGetDBInstance() async {
+Future<dynamic> firebaseGetDBInstance() async {
   print("Google Firebase认证功能已被禁用");
   return null;
 }
